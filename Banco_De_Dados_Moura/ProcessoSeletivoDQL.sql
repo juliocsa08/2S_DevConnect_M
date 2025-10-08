d2b5663d35a49fce363c83267f0c2a936eaf5823
Julio Cesar <jubuchin2@gmail.com>
2025-10-08 08:36:56 -0300

    Processo Seletivo Devconnect

---
+++ Banco_De_Dados_Moura/ProcessoSeletivoDQL.sql
@@ -0,0 +1,21 @@
+--DQL
+USE db_Devconnect
+
+--USUARIOS
+SELECT * FROM tb_usuario;
+
+
+--PUBLICACAO
+SELECT * FROM tb_publicacao;
+
+
+--CURTIDA
+SELECT * FROM tb_curtida;
+
+
+--COMENTARIO
+SELECT * FROM tb_comentario;
+
+
+--SEGUIDORES
+SELECT * FROM tb_seguidor;
